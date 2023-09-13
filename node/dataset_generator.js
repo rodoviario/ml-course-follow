b01c1e93761b2dd04e35f812f1e5cc9394cbd356
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2023-09-13 19:21:28 +0000

    feat: image creation

--- node/dataset_generator.js
+++ node/dataset_generator.js
@@ -1,34 +1,61 @@
-const constants={};
+const draw=require('../common/draw.js');
 
-constants.DATA_DIR="../data";
-constants.RAW_DIR=constants.DATA_DIR+"/raw";
-constants.DATASET_DIR=constants.DATA_DIR+"/dataset";
-constants.JSON_DIR=constants.DATASET_DIR+"/json";
-constants.IMG_DIR=constants.DATASET_DIR+"/img";
-constants.SAMPLES=constants.DATASET_DIR+"/samples.json";
+const {createCanvas}=require('canvas');
+const canvas=createCanvas(400,400);
+const ctx=canvas.getContext('2d');
 
-const fs=require('fs');
+const constants = {};
+
+constants.DATA_DIR = "../data";
+constants.RAW_DIR = constants.DATA_DIR + "/raw";
+constants.DATASET_DIR = constants.DATA_DIR + "/dataset";
+constants.JSON_DIR = constants.DATASET_DIR + "/json";
+constants.IMG_DIR = constants.DATASET_DIR + "/img";
+constants.SAMPLES = constants.DATASET_DIR + "/samples.json";
+
+const fs = require('fs');
 
 const fileNames=fs.readdirSync(constants.RAW_DIR);
 const samples=[];
 let id=1;
 fileNames.forEach(fn=>{
-    const content=fs.readFileSync(
-        constants.RAW_DIR+'/'+fn
+  const content=fs.readFileSync(
+    constants.RAW_DIR+"/"+fn
+  );
+  const {session,student,drawings}=JSON.parse(content);
+  for(let label in drawings){
+    samples.push({
+      id,
+      label,
+      student_name:student,
+      student_id:session
+    });
+
+    const paths=drawings[label];
+    fs.writeFileSync(
+      constants.JSON_DIR+"/"+id+".json",
+      JSON.stringify(paths)
     );
-    const {session, student, drawings}=JSON.parse(content);
-    for(let label in drawings) {
-        samples.push({
-            id,
-            label,
-            student_name:student,
-            student_id:session
-        });
-        id++;
-    }
+
+    generateImageFile(
+      constants.IMG_DIR+"/"+id+".png",
+      paths
+    );
+
+    id++;
+  }
 });
 
-fs.writeFileSync(
-    constants.SAMPLES,
-    JSON.stringify(samples)
+fs.writeFileSync(constants.SAMPLES,
+  JSON.stringify(samples)
 );
+
+function generateImageFile(outFile,paths){
+  ctx.clearRect(0,0,
+    canvas.width,canvas.height
+  );
+  draw.paths(ctx,paths);
+
+  const buffer=canvas.toBuffer("image/png");
+  fs.writeFileSync(outFile,buffer);
+}
