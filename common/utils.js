b9fe017cd9d6ff1a226134086685a84454e4c8ef
Rodolfo Guagnini <rodolfo.guagnini@gmail.com>
2023-09-25 19:49:27 +0000

    wip: graph improvements

--- common/utils.js
+++ common/utils.js
@@ -23,14 +23,14 @@ utils.flaggedUsers=
 ];
 
 utils.styles={
-  car:'gray',
-  fish: 'red',
-  house: 'yellow',
-  tree: 'green',
-  bicycle: 'cyan',
-  guitar: 'blue',
-  pencil: 'magenta',
-  clock: 'lightgray',
+  car: {color:'gray', text:'🚗'},
+  fish: {color:'red', text:'🐠'},
+  house: {color:'yellow', text:'🏠'},
+  tree: {color:'green', text:'🌳'},
+  bicycle: {color:'cyan', text:'🚲'},
+  guitar: {color:'blue', text:'🎸'},
+  pencil: {color:'magenta', text:'✏️'},
+  clock: {color:'lightgray', text:'🕒'},
 }
 
 
